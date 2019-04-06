7fb78bdb1e26b6debea92a61189233287983d9d2
andika <andikayudantara98@gmail.com>
2019-04-06 11:30:12 +0700

    baru

---
+++ assets/js/home.js
@@ -0,0 +1,37 @@
+// Get the moda
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+alert("AA");
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var slideIndex = 0;
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  	slideIndex++;
+  	if (slideIndex > x.length) {
+  		slideIndex = 1
+  	} 
+  	x[slideIndex-1].style.display = "block"; 
+  	setTimeout(carousel, 3000);
+}
+
