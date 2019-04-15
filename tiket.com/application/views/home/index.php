68f73921486fdfe372156ab6e3c934bb8fbe36ca
andika <andikayudantara98@gmail.com>
2019-04-15 21:53:50 +0700

    tes

--- tiket.com/application/views/home/index.php
+++ tiket.com/application/views/home/index.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="assets/css/home.css">
-	<script type="text/javascript" src="assets/js/home.js"></script>
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</head>
-
-<body body onload="carousel()">
-	<img class="logo" href="#" src="assets/img/logo.svg">
-	<div class="navbar-top">
+<div onload="carousel()" class="container-fluid text-center" style="border: 0.5px solid #e6e6e6;">
+<img class="logo" href="#" src="assets/img/logo.svg">
+	<!-- <div class="navbar-top">
 		<nav class="navbar navbar-expand-sm">		
   			<ul  class="navbar-nav">
   				<div class="dropdown">
@@ -44,7 +30,7 @@
 	   			<li id="prom" class="nav-item"><a class="nav-link" href="#">TIX Point</a></li>
 	  		</ul>
 		</nav>	
-	</div>
+	</div> -->
 
 	<div class="slides">
   		<img class="mySlides" src="assets/img/1.jpg" style="width:100%">
@@ -80,5 +66,5 @@
 		</div>
 		
 	</div>
-</body>
-</html>
+</div>
+
