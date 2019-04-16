9497e4dbee122c361376d378c884d73453865cef
gerryfernando <gerryfernando21@gmail.com>
2019-04-16 09:15:33 +0700

    footer

--- tiket.com/application/views/login/index.php
+++ tiket.com/application/views/login/index.php
@@ -7,18 +7,26 @@
 	</div>
 	<div class="row justify-content-center" style="font-size: 14px;">
 		<?= form_open(base_url('Login/validate')); ?>
-		<form class="rounded" method="post" style="width: 498px; border: 2px solid #e6e6e6; padding: 40px 40px; margin: 15px;">
-			<div class="form-group text-left">
-				<label>Alamat Email</label>
-				<input type="email" class="form-control" name="email">
+		<form class="rounded" method="post" id="form1" style="width: 498px; border: 2px solid #e6e6e6; padding: 40px 40px; margin: 15px;">
+		  <div style="border: solid 1px; padding: 50px;">
+			<div class="form-group text-left ">
+				<input  type="email" class="form-control border-right-0 border-top-0 border-left-0" placeholder="Alamat Email" name="email">
 			</div>
-			<div class="form-group text-left">
-				<label>Kata Sandi</label>
-				<input type="password" class="form-control" name="password">
+			<div class="form-group text-left" >
+				<input type="password"  placeholder="Kata Sandi" class="form-control border-right-0 border-top-0 border-left-0" name="password">
 			</div>
 			<div class="form-group"><a class="disabled" style="color: #FF7200; font-size: 13px">Lupa Kata Sandi?</a></div>
 			<button type="submit" name="login" class="btn btn-primary" style="background-color: #FF7200; width: 415px; margin: auto auto 15px;">Log In</button>
 			<div class="form-group text-center">Belum punya akun? <a href="<?= base_url('Register'); ?>" style="color: #FF7200">Daftar</a></div>
+			</div>	
 		</form>
 	</div>
 </div>
+
+
+<br>
+<br>
+<br>
+
+		<img style="display: block; margin-left: auto;margin-right: auto;" src="<?= base_url(); ?>/assets/img/logo_log_bot.png" >
+
--- tiket.com/application/views/templates/footer.php
+++ tiket.com/application/views/templates/footer.php
@@ -1,5 +1,177 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css"  href="<?= base_url(); ?>/assets/css/footer.css">
+
+	
+	<footer class="page-footer font-small stylish-color-dark pt-4">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+     
+
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col mx-auto">
+
+          <!-- Links -->
+          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Perusahaan</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!"> Karir</a>
+            </li>
+            <li>
+              <a href="#!">Corporate 2</a>
+            </li>
+            <li>
+              <a href="#!">Partner</a>
+            </li>
+            <li>
+              <a href="#!">Blog</a>
+            </li>
+             <li>
+              <a href="#!">Aplikasi Mobile</a>
+            </li>
+             <li>
+              <a href="#!">Perlindungan</a>
+            </li>
+             <li>
+              <a href="#!">Cicilan</a>
+            </li>
+             <li>
+              <a href="#!">Daftarkan Hotel Anda</a>
+            </li>
+          </ul>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col mx-auto">
+
+          <!-- Links -->
+          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Bantuan</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">FAQ</a>
+            </li>
+            <li>
+              <a href="#!">Kebijakan dan Privasi</a>
+            </li>
+            <li>
+              <a href="#!">Syarat dan Ketentuan</a>
+            </li>
+          </ul>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col mx-auto">
+
+          <!-- Links -->
+          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Penghargaan</h5>
+
+         <img src="<?= base_url(); ?>/assets/img/logo_penghargaan.png">
+        </div>
+        <!-- Grid column -->
+         <hr class="clearfix w-100 d-md-none">
+
+        <!-- Grid column -->
+        <div class="col mx-auto">
+
+          <!-- Links -->
+        
+          <ul>
+          	 <li>
+         		<img style="margin-bottom: 20px;" src="<?= base_url(); ?>/assets/img/logo_CC.png">
+     	 	 </li>
+     	 	 <li style="margin-bottom: 10px;font-weight: bold;">
+     	 	 	<p>Ada Pertanyaan</p>
+     	 	 </li>
+     	 	 <li>
+     	 	 	<div class="input-group mb-3">
+ 						 <input style="font-size: 87%" type="text" class="form-control" placeholder="Masukan Pertanyaan">
+ 				 		<div class="input-group-append">
+   								 <span style="background-color: #ff6600;color: white;" class="input-group-text" id="basic-addon2">Kirim</span>
+ 						 </div>
+				</div>
+     	 	 </li>
+     	 </ul>
+        </div>
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <hr>
+
+</footer>
+<footer class="page-footer font-small cyan darken-3">
+
+    <!-- Footer Elements -->
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-12 py-5">
+          <div class="mb-5 flex-center">
+
+            <!-- Facebook -->
+            <a class="fb-ic">
+              <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!-- Twitter -->
+            <a class="tw-ic">
+              <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!-- Google +-->
+            <a class="gplus-ic">
+              <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Linkedin -->
+            <a class="li-ic">
+              <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Instagram-->
+            <a class="ins-ic">
+              <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Pinterest-->
+            <a class="pin-ic">
+              <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+            </a>
+          </div>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+
+    </div>
+    <!-- Footer Elements -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
 </body>
 </html>
